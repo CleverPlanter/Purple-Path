4033b93a804635e325a5f3dffaa382e37ec1d05c
bbliss503 <bbliss1234@gmail.com>
2024-08-02 22:30:49 -0400

    Pet the Cat & IMPORTANT
    Mostly finished some more pet the cat assets and created some printed win/loss conditions
    
    Oh VERY IMPORTANT: I moved the primary mini game scenes into the minigame scenes folder. Currently this is the only way to get them to work with the trigger zones, everything else can live outside of this folder

---
+++ Minigame Manager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_game_timer_timeout():
+	print ("Timer End")
+	pass # Replace with function body.
--- Scene/Pet the Cat/pet_the_cat_script.gd
+++ Scene/Pet the Cat/pet_the_cat_script.gd
@@ -5,16 +5,19 @@ extends Node
 
 var petCounter: int
 
+var win: bool
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	win = false
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if petCounter >= 10:
+	if petCounter == 10:
+		win = true
 		print("Winner!")
-		queue_free()
 	pass
 
 func _on_left_pet_zone_mouse_entered():
@@ -27,3 +30,10 @@ func _on_left_pet_zone_mouse_entered():
 func _on_right_pet_zone_mouse_entered():
 	print ("right hover")
 	pass # Replace with function body.
+
+
+func _on_timer_timeout():
+	if (win == true):
+		print ("You Win!") 
+	elif (win == false):
+		print ("You Lose!")
